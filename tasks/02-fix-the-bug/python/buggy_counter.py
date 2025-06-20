8729eb79f5989cc49863026c51894f5d57548fa3
kyawsawshein <xealkssshein@gmail.com>
2025-06-20 13:49:35 +0700

    Task 02 Fixed the concurrent get global value with thread lock

--- tasks/02-fix-the-bug/python/buggy_counter.py
+++ tasks/02-fix-the-bug/python/buggy_counter.py
@@ -4,11 +4,20 @@ import threading
 import time
 
 _current = 0
+# using the thread Lock
+_t_lock = threading.Lock()
+
 
 def next_id():
     """Returns a unique ID, incrementing the global counter."""
+    # may be get the same time and duplicate value when run in thread
     global _current
-    value = _current
-    time.sleep(0)
-    _current += 1
-    return value
+    # using the thread Lock that lock the thread access and then release Lock
+    # using with to guarantee release the lock
+    with _t_lock:
+        value = _current
+        time.sleep(0)
+        _current += 1
+        return value
+
+#Solution : Using the thread Lock the controle the thread lock and release
