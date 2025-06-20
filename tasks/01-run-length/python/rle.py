eebdefe056f0fe5f51686cc1ef27221bca507350
kyawsawshein <xealkssshein@gmail.com>
2025-06-20 13:44:50 +0700

    Task 01 encod the charactor and count using the groupby

--- tasks/01-run-length/python/rle.py
+++ tasks/01-run-length/python/rle.py
@@ -1,3 +1,6 @@
+from itertools import groupby
+
+
 def encode(s: str) -> str:
     """
     Run‑length encode the input string.
@@ -5,4 +8,10 @@ def encode(s: str) -> str:
     >>> encode("AAB") -> "A2B1"
     """
     # TODO: implement
-    raise NotImplementedError("Implement me!")
+    # raise NotImplementedError("Implement me!")
+
+    # raise NotImplementedError("Implement me!")
+    # grouping the string like ("a", generator value) and then sum the gererator value
+    return "".join(f"{char}{sum(1 for _ in grp)}" for char, grp in groupby(s))
+
+# Solution : Using groupby to group the element with generator value and sum the value
